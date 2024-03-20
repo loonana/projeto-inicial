33ba2cf3dd8b63c869a160c866e11d7cca885124
loonana <itsloye@gmail.com>
2024-03-20 09:51:38 -0300

    formulario

--- scripts.js
+++ scripts.js
@@ -1,7 +1,15 @@
 var botao = document.getElementById("botao"); 
 botao.addEventListener("click", alertar, false); 
-var nome = document.getElementById("nome"); 
-
-function alertar(event){
-    alert("Você clicou no botão!" + " " + nome.value); 
-}
+var nome = document.getElementById("nome");
+var email = document.getElementById("email"); 
+var saida = document.getElementById("saida-de-dados");
+var telefone = document.getElementById("telefone"); 
+var cep = document.getElementById("telefone"); 
+var logradouro = document.getElementById("logradouro"); 
+var numero = document.getElementById("numero"); 
+var complemento = document.getElementById("complemento"); 
+var bairro = document.getElementById("bairro"); 
+var cidade = document.getElementById("cidade"); 
+var estado = document.getElementById("estado"); 
+// function alertar(event){
+//     alert("Você clicou no botão!" + " " + nome.value);}
